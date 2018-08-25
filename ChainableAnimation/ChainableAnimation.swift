b3f4e364620c887516c5fc8dd1d036ddc8b590a0
atsushi130 <atsushi_main@outlook.jp>
2018-08-25 18:34:34 +0900

    add: ChainableAnimation class

---
+++ ChainableAnimation/ChainableAnimation.swift
@@ -0,0 +1,16 @@
+//
+//  ChainableAnimation.swift
+//  ChainableAnimation
+//
+//  Created by Atsushi Miyake on 2018/08/25.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+final class ChainableAnimation {
+    
+    private var next: ChainableAnimation? = nil
+    private var prev: ChainableAnimation? = nil
+
+}
